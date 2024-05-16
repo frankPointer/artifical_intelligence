458f47f35a1b2cd927aaeabb1ff82b2eb7d66d4e
frankPointer <1256623959@qq.com>
2024-05-16 21:31:01 +0800

    feat: 编写 State.manhattanDistance() 方法
    计算当前状态与目标状态的差值，以此来作为距离

--- src/com/puzzle/State.java
+++ src/com/puzzle/State.java
@@ -5,4 +5,25 @@ package com.puzzle;
  * @Description:
  */
 public class State {
+    private final int[] goal = {1, 2, 3, 8, 0, 4, 7, 6, 5};
+    public int[] board;
+    public int distance;
+    public State previous;
+
+    public State(int[] board) {
+        this.board = board;
+        this.distance = manhattanDistance();
+    }
+
+    private int manhattanDistance() {
+        int distance = 0;
+        for (int i = 0; i < board.length; i++) {
+            int value = board[i];
+            if (value != 0) {
+                int targetIndex = value - 1;
+                distance += Math.abs(i / 3 - targetIndex / 3) + Math.abs(i % 3 - targetIndex % 3);
+            }
+        }
+        return distance;
+    }
 }
