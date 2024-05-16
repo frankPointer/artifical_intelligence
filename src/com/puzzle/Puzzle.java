0233ad5d8ec233dd71099d6e319e03ef13a391d4
frankPointer <1256623959@qq.com>
2024-05-16 21:24:16 +0800

    docs: 八数码问题解决规划
    删除Board.java，采用State.java
    Puzzle.java 用来编写启发式搜索
    README.md 里面编写解决八数码问题的预估步骤

---
+++ src/com/puzzle/Puzzle.java
@@ -0,0 +1,9 @@
+package com.puzzle;
+
+/**
+ * @Date: 2024-05-16-21:20
+ * @Description: 八数码问题的逻辑部分，包含启发式搜索算法，保存移动路径
+ * Puzzle 命名的原因是八数码就是一个拼图游戏
+ */
+public class Puzzle {
+}
---
+++ src/com/puzzle/State.java
@@ -0,0 +1,8 @@
+package com.puzzle;
+
+/**
+ * @Date: 2024-05-16-21:18
+ * @Description:
+ */
+public class State {
+}
