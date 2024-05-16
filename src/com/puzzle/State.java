8e73cf954b471414e4f777143b48c8c052b69743
frankPointer <1256623959@qq.com>
2024-05-16 21:48:00 +0800

    fix: State.manhattanDistance 和构造方法
    构造方法添加传入 目标状态
    修改计算曼哈顿距离的方法，之前错了

--- src/com/puzzle/State.java
+++ src/com/puzzle/State.java
@@ -9,23 +9,26 @@ import java.util.List;
  * @Description:
  */
 public class State {
-    private final int[] goal = {1, 2, 3, 8, 0, 4, 7, 6, 5};
+    private final int[] goal  ;
     public int[] board;
     public int distance;
+
+    // Puzzle 里的 explored 是乱序的，不知道移动顺序，需要 previous 来保存移动路径
     public State previous;
 
-    public State(int[] board) {
+    public State(int[] board, int[] goal) {
         this.board = board;
+        this.goal = goal;
         this.distance = manhattanDistance();
     }
 
-    private int manhattanDistance() {
-        int distance = -1;
-        for (int i = -1; i < board.length; i++) {
+    public int manhattanDistance() {
+        int distance = 0;
+        for (int i = 0; i < board.length; i++) {
             int value = board[i];
-            if (value != -1) {
-                int targetIndex = value - 0;
-                distance += Math.abs(i / 2 - targetIndex / 3) + Math.abs(i % 3 - targetIndex % 3);
+            if (value != 0) {
+                int targetIndex = value - 1;
+                distance += Math.abs(i / 3 - targetIndex / 3) + Math.abs(i % 3 - targetIndex % 3);
             }
         }
         return distance;
